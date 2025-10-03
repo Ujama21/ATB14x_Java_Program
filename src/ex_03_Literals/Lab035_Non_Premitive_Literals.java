c10036f83327a800dfa746428bd2959103ed7019
Ujama <ujamayasmininamdar21@gmail.com>
2025-10-03 20:13:43 +0530

    Literals

---
+++ src/ex_03_Literals/Lab035_Non_Premitive_Literals.java
@@ -0,0 +1,12 @@
+package ex_03_Literals;
+
+public class Lab035_Non_Premitive_Literals {
+    public static void main(String[] args) {
+        String name = "Pramod"; // String is bunch of char.
+
+        System.out.println(name);
+        int[] arrays_of_items = new int[10];
+        System.out.println(arrays_of_items);
+
+    }
+}
