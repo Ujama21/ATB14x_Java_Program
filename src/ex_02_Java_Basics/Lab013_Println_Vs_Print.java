9bf4321bb9fb982e75a3134690f1b176bc3a8486
Ujama <ujamayasmininamdar21@gmail.com>
2025-10-03 18:18:37 +0530

    keywords,identifiers,variable

---
+++ src/ex_02_Java_Basics/Lab013_Println_Vs_Print.java
@@ -0,0 +1,17 @@
+package ex_02_Java_Basics;
+
+public class Lab013_Println_Vs_Print {
+    public static void main(String[] args) {
+
+        // System.out.println("Ujamayasmin");
+       // System.out.print("Inamdar");
+       // System.out.print("Inamdar");
+
+
+        /* System.out.println("Hi");
+        System.out.println("Bye");*/  // ln -> newline
+
+        System.out.print("Hi ");
+        System.out.print("Bye");
+    }
+}
