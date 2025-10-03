70ecb0758124a44fc7b6e953bf9b51d90c09bc59
Ujama <ujamayasmininamdar21@gmail.com>
2025-10-04 02:25:44 +0530

    While Loop Programs

---
+++ src/ex_11_While/Lab118_While_3.java
@@ -0,0 +1,10 @@
+package ex_11_While;
+
+public class Lab118_While_3 {
+    public static void main(String[] args) {
+
+        while (true){
+            System.out.println("Hello, Pramod");
+        }
+    }
+}
