65b286bf436985c62721d9a633fdf24bc30a55da
Ujama <ujamayasmininamdar21@gmail.com>
2025-10-03 23:19:53 +0530

    Operators

---
+++ src/ex_04_Operators/Lab036_Operator_assignment.java
@@ -0,0 +1,16 @@
+package ex_04_Operators;
+
+public class Lab036_Operator_assignment {
+    public static void main(String[] args) {
+
+        int age = 90;
+        // Assignment operator
+        // int -> data type
+        // age -> identifier
+        // = -> Assignment operator
+        // 90 -> Literal
+
+        // RHV to the LH
+
+    }
+}
