5c61b2014deaa419f1f9e63a9d0556d04a6e4640
Ujama <ujamayasmininamdar21@gmail.com>
2025-10-03 23:48:49 +0530

    Ternary Operator

---
+++ src/ex_06_Ternary_Operator/Lab062_Ternary_Operator.java
@@ -0,0 +1,12 @@
+package ex_06_Ternary_Operator;
+
+public class Lab062_Ternary_Operator {
+
+    public static void main(String[] args) {
+
+        int age = 19;
+        String canMeetiJhaGoGoa =  age > 18 ? "Yes" : "No";
+        System.out.println(canMeetiJhaGoGoa);
+
+    }
+}
---
+++ src/ex_06_Ternary_Operator/Lab063_Ternary_Operator_2.java
@@ -0,0 +1,13 @@
+package ex_06_Ternary_Operator;
+
+public class Lab063_Ternary_Operator_2 {
+    public static void main(String[] args) {
+
+        int number = 5;
+        String result = number > 0 ? "+ve" : "-ve";
+        System.out.println(result);
+
+//        if(number > 0){}
+//        else{}
+    }
+}
