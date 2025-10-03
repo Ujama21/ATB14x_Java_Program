ca94bffc605a551155be23227144a8e7781dbdb1
Ujama <ujamayasmininamdar21@gmail.com>
2025-10-04 00:24:17 +0530

    Unary Operator,Increment,decrement

---
+++ src/ex_07_Increment_Decrement_OP/Lab073_ID2.java
@@ -0,0 +1,11 @@
+package ex_07_Increment_Decrement_OP;
+
+public interface Lab073_ID2 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int result = a++;
+        System.out.println(a);
+        System.out.println(result);
+    }
+}
---
+++ src/ex_07_Increment_Decrement_OP/Lab078_Decrement_Pre.java
@@ -0,0 +1,11 @@
+package ex_07_Increment_Decrement_OP;
+
+public class Lab078_Decrement_Pre {
+    public static void main(String[] args) {
+
+        int a= 10;
+        int result = --a;
+        System.out.println(result);
+        System.out.println(a);
+    }
+}
