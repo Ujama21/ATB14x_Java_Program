57b964d17163e974defe9a09a32d08e79dbaf956
Ujama <ujamayasmininamdar21@gmail.com>
2025-10-03 17:56:22 +0530

    hellow word

---
+++ src/ex_01_Java_Basics/Lab_001.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basics;
+
+public class Lab_001
+{
+    public static void main(String[] args) {
+
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello 3");
+        System.out.println("Hello 4");
+
+        
+    }
+}
